79d396f2dc03de4f99ff716ebb72797e2ac8af07
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-10-17 13:31:51 +0900

    update document

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,27 @@ function App() {
     <>
       <div className="font-sample noto-sans-japanese">
         <div className="font-title">Noto Sans Japanese</div>
-        <div className="sample-text">グランピアン山の奥、ブリタニアの深い霧、赤く輝くダンディー港、雨に濡れるロンドンの街角。</div>
+        <div className="sample-text">あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。</div>
       </div>
 
       <div className="font-sample noto-serif-japanese">
         <div className="font-title">Noto Serif Japanese</div>
-        <div className="sample-text">グランピアン山の奥、ブリタニアの深い霧、赤く輝くダンディー港、雨に濡れるロンドンの街角。</div>
+        <div className="sample-text">あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。</div>
       </div>
 
       <div className="font-sample m-plus-1p">
         <div className="font-title">M PLUS 1p</div>
-        <div className="sample-text">グランピアン山の奥、ブリタニアの深い霧、赤く輝くダンディー港、雨に濡れるロンドンの街角。</div>
+        <div className="sample-text">あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。</div>
       </div>
 
       <div className="font-sample zen-kaku-gothic-new">
         <div className="font-title">Zen Kaku Gothic New</div>
-        <div className="sample-text">グランピアン山の奥、ブリタニアの深い霧、赤く輝くダンディー港、雨に濡れるロンドンの街角。</div>
+        <div className="sample-text">あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。</div>
       </div>
 
       <div className="font-sample dela-gothic-one">
         <div className="font-title">Dela Gothic One</div>
-        <div className="sample-text">グランピアン山の奥、ブリタニアの深い霧、赤く輝くダンディー港、雨に濡れるロンドンの街角。</div>
+        <div className="sample-text">あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。</div>
       </div>
     </>
   )
